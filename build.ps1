f1e5c5944864db1dc935fd023fb223759d95857a
Stephen Pence <sjpence00@gmail.com>
2016-02-11 23:36:19 -0500

    Add template and build script for pdf

---
+++ build.ps1
@@ -0,0 +1,6 @@
+pandoc .\01_Introduction.md .\02_FacilityConnections.md .\03_Installation.md .\04_InitialStartUp.md .\05_Operation.md .\06_Maintenance.md .\07_ServiceProcedures.md `
+--toc --template=default.latex --toc-depth=2 --chapters `
+-V geometry:margin=1in `
+-s -o manual.pdf
+
+# Move-Item .\manual.pdf -Destination 
