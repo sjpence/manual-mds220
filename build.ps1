520f46b6c9a21a2c24e6038d83c19b24e81ca48a
Stephen Pence <sjpence00@gmail.com>
2016-02-11 23:53:14 -0500

    Update build script
    -Remove previous version if present
    -Move new version to build

--- build.ps1
+++ build.ps1
@@ -1,6 +1,10 @@
+if(Test-Path .\build\manual.pdf){
+    Remove-Item .\build\manual.pdf
+}
+
 pandoc .\01_Introduction.md .\02_FacilityConnections.md .\03_Installation.md .\04_InitialStartUp.md .\05_Operation.md .\06_Maintenance.md .\07_ServiceProcedures.md `
 --toc --template=default.latex --toc-depth=2 --chapters `
 -V geometry:margin=1in `
 -s -o manual.pdf
 
-# Move-Item .\manual.pdf -Destination 
+Move-Item .\manual.pdf -Destination .\build\
